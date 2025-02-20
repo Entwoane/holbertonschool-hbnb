c2de0c37554a380fc847f5b099d6d346cd4882f3
Benji021 <9548holbertonstudents.com>
2025-02-20 16:42:42 +0100

    User Class

--- part2/app/models/user.py
+++ part2/app/models/user.py
@@ -2,15 +2,16 @@
 """Define class user"""
 
 
+import re
 import uuid
 from datetime import datetime
 
 class user:
-    def __init__(self):
+    def __init__(self, first_name, last_name, email, is_admin=False):
         self.id = str(uuid.uuid4())
-        self.first_name = str()
-        self.last_name = str()
-        self.email = str()
-        self.is_admin = ()
+        self.first_name = self.validate_name(first_name, "Prénom")
+        self.last_name = self.validate_name(last_name, "Nom de famille")
+        self.email = self.validate_email(email)
+        self.is_admin = is_admin
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
