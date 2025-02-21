0b389951b25208cceb79dbfaf3667c36bc697a42
Benji021 <9548holbertonstudents.com>
2025-02-21 14:45:51 +0100

    Define Class Aminity

---
+++ part2/app/models/amenity.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Define Class Aminity """
+
+
+import uuid
+from datetime import datetime
+
+class Amenity:
+    def __init__(self, name: str):
+        if len(name) > 50:
+            raise ValueError("Equipment name must not exceed 50 characters")
+
+        # Generate a unique ID
+        self.id = str(uuid.uuid4())
+        self.name = name
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def update(self, new_name: str):
+        """ Update equipment name """
+        if len(new_name) > 50:
+            raise ValueError("Equipment name must not exceed 50 characters")
+
+        self.name = new_name
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return f"Amenity(id={self.id}, name={self.name}, created_at={self.created_at}, updated_at={self.updated_at})"
