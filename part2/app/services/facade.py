44fe16ab1f4d7babc200299e7a29c65a3715de1a
Benji021 <9548holbertonstudents.com>
2025-02-26 16:34:54 +0100

    Hbnb Façade

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,18 +1,42 @@
 from app.persistence.repository import InMemoryRepository
+from app.models.amenity import Amenity
+
 
 class HBnBFacade:
     def __init__(self):
         self.user_repo = InMemoryRepository()
-        self.place_repo = InMemoryRepository()
-        self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
 
-    # Placeholder method for creating a user
     def create_user(self, user_data):
-        # Logic will be implemented in later tasks
-        pass
+        user = User(**user_data)
+        self.user_repo.add(user)
+        return user
 
-    # Placeholder method for fetching a place by ID
-    def get_place(self, place_id):
-        # Logic will be implemented in later tasks
-        pass
+    def get_user(self, user_id):
+        return self.user_repo.get(user_id)
+
+    def get_user_by_email(self, email):
+        return self.user_repo.get_by_attribute('email', email)
+
+    def create_amenity(self, amenity_data):
+        if not amenity_data or 'name' not in amenity_data:
+            return None
+
+        new_amenity = Amenity(amenity_data["name"])
+        self.amenity_repo.add(new_amenity)
+        return new_amenity
+
+    def get_amenity(self, amenity_id):
+        return self.amenity_repo.get(amenity_id)
+
+    def get_all_amenities(self):
+        return self.amenity_repo.get_all()
+
+    def update_amenity(self, amenity_id, amenity_data):
+        amenity = self.amenity_repo.get(amenity_id)
+        if not amenity or 'name' not in amenity_data:
+            return None
+
+        amenity.update(amenity_data["name"])
+        self.amenity_repo.update(amenity_id, amenity)
+        return amenity
