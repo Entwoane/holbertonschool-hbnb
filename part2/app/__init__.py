5fbf1efe6d56a528d437c87e80f4659ab4e8a67b
Benji021 <9548holbertonstudents.com>
2025-02-27 13:59:44 +0100

    init

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
+from app.api.v1.amenities import api as amenities_ns
 
 def create_app():
     app = Flask(__name__)
@@ -9,3 +10,11 @@ def create_app():
     # Register the users namespace
     api.add_namespace(users_ns, path='/api/v1/users')
     return app
+
+app = Flask(__name__)
+api = Api(app)
+
+# Ajouter le namespace
+api.add_namespace(amenities_ns, path="/api/v1/amenities")
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)  
