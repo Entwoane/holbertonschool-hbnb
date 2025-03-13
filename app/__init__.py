80b9e48ce56eac410620b60119a59d00ac7756b1
Notsayy <jtertre25@gmail.com>
2025-03-13 22:34:21 +0100

    Task 2: Update files , will need a little debug

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,18 @@
 from flask import Flask
 from flask_restx import Api
 from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
 from flask_bcrypt import Bcrypt
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
+from app.api.v1.auth import api as auth_api
+from app.api.v1.protected import api as protected_api
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
-
+jwt = JWTManager()
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
@@ -18,11 +21,14 @@ def create_app(config_class="config.DevelopmentConfig"):
 
     db.init_app(app)
     bcrypt.init_app(app)
+    jwt.init_app(app)
     # Register the users namespace
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path="/api/v1/amenities")
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    api.add_namespace(auth_api, path='/api/v1/auth')
+    api.add_namespace(protected_api, path='/api/v1/protected')
 
     return app
 
